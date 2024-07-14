5e400f3c017607dabdcb3b78ae2c27f79d51ece3
HSH2005 <161571636+HSH2005@users.noreply.github.com>
2024-07-14 11:21:54 +0800

    Update movie database.py

--- resit/9/movie database.py
+++ resit/9/movie database.py
@@ -1,10 +1,34 @@
-import xml.dom.minidom
-DOMTree = xml.dom.minidom.parse("movies.xml")
-collection = DOMTree.documentElement
-movies = collection.getElementsByTagName('Movie')
-collection2 = movies.documentElement
-titles = collection2.getElementsByTagName('Title')
-for movie in movies: 
-    print(movie.getElementsByTagName('Title')[0].firstChild.nodeValue, movie.getElementsByTagName('Year')[0].firstChild.nodeValue, movie.getElementsByTagName('Genre')[0].firstChild.nodeValue)
-for title in titles:
-    print(title.getElementsByTagName('runtime')[0].firstChild.nodeValue)
+import xml.etree.ElementTree as ET
+
+class Movie:
+    def __init__(self, title, runtime, year, genre):
+        self.title = title
+        self.runtime = runtime
+        self.year = year
+        self.genre = genre
+
+    def print_details(self):
+        print(f"Title: {self.title}, Runtime: {self.runtime} minutes, Year: {self.year}, Genre: {self.genre}")
+
+def parse_movies_from_xml(xml_file):
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+    movies = []
+    for movie_elem in root.findall('Movie'):
+        title_elem = movie_elem.find('Title')
+        genre_elem = movie_elem.find('Genre')
+        year_elem = movie_elem.find('Year')
+        runtime = int(title_elem.get('runtime', 0))
+        title = title_elem.text
+        genre = genre_elem.text
+        year = int(year_elem.text)
+        movie = Movie(title, runtime, year, genre)
+        movies.append(movie)
+
+    return movies
+
+xml_file_path = './movies.xml'
+movies_list = parse_movies_from_xml(xml_file_path)
+
+for movie in movies_list:
+    movie.print_details()
